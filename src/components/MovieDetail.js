f4fdcb4a702758400823e2bf4ef798e803ef7ce2
Brandon <brandon121j@gmail.com>
2021-10-26 23:10:15 -0400

    Assignment complete for the most part, just need to fix auto refresh issue

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import propTypes from "prop-types";
 
-function movieDetail({ 
+function MovieDetail({ 
     title,
     year,
     rated,
     genre,
-    director,
-    writer,
     plot,
     language,
     country,
     awards,
-    poster
+    poster,
+    runtime
 }) {
     
     return(
-
         <div className="movieDetailsContainer">
             <div className="listedMovieDetails">
                 <h1>Title: {title} ({year})</h1>
                 <img src={poster} alt=""/>
-                <p>Plot: {plot}</p>
+                <p>Plot:</p> 
+                <p>{plot}</p>
                 <p>Genre: {genre}</p>
+                <p>Runtime: {runtime}</p>
                 <p>Rating: {rated}</p>
-                <p>Director: {director}</p>
-                <p>Writer: {writer}</p>
                 <p>Language: {language}</p>
                 <p>Country: {country}</p>
                 <p>Awards: {awards}</p>
@@ -34,18 +32,17 @@ function movieDetail({
     )
 }
 
-movieDetail.propTypes = {
+MovieDetail.propTypes = {
     title: propTypes.string.isRequired,
     year: propTypes.string.isRequired,
     rated: propTypes.string.isRequired,
     genre: propTypes.string.isRequired,
-    director: propTypes.string.isRequired,
-    writer: propTypes.string.isRequired,
     plot: propTypes.string.isRequired,
     language: propTypes.string.isRequired,
     country: propTypes.string.isRequired,
     awards: propTypes.string.isRequired,
-    poster: propTypes.string.isRequired
+    poster: propTypes.string.isRequired,
+    runtime: propTypes.string.isRequired
 }
 
-export default movieDetail;
+export default MovieDetail;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,114 +1,114 @@
 import React, { Component } from "react";
 import axios from "axios";
+import MovieDetail from "./MovieDetail";
 import Loading from "./Loading";
-import MovieDetail from "./MovieDetail"
 
 class Search extends Component {
-
 	state = {
-        isError: false,
-        errorMessage: "",
-        isLoading: false,
-        movieSearch: "",
-        title: "",
-        year: "",
-        rated: "",
-        genre: "",
-        director: "",
-        writer: "",
-        plot: "",
-        language: "",
-        country: "",
-        awards: "",
-        poster: "",
-        initialSearch: "Shrek"
-    }
+		movieSearch: "",
+		title: "",
+		year: "",
+		rated: "",
+		genre: "",
+		plot: "",
+		language: "",
+		country: "",
+		awards: "",
+		poster: "",
+        runtime: "",
+		initialSearch: "shrek",
+		isError: false,
+		errorMessage: "",
+		isLoading: false,
+	};
 
+	async componentDidMount() {
+		this.fetchMovies(this.state.initialSearch);
+	}
 
-    async componentDidMount() {
-        this.fetchMovies(this.state.initialSearch)
-    }
+	fetchMovies = async (movieSearch) => {
+		this.setState({
+			isLoading: true,
+		});
 
-    fetchMovies = async(movieSearch) => {
-
-        this.setState({
-            isLoading: true
-        });
-
-        try {
-            let result = await axios.get(`https://www.omdbapi.com/?apikey=efbef9a0&t=${movieSearch}`);
-            this.setState({
-                movieSearch: result.data.movieSearch,
-                title: result.data.title,
-                year: result.data.year,
-                rated: result.data.rated,
-                genre: result.data.genre,
-                director: result.data.director,
-                writer: result.data.writer,
-                plot: result.data.plot,
-                language: result.data.language,
-                country: result.data.country,
-                awards: result.data.awards,
-                poster: result.data.poster,
-                isError: false,
-                isLoading: false
-            })
-        
-        
-        } catch(error) {
-            console.log(error.response);
-            if (error && error.response === 404) {
-                this.setState({
+		try {
+			let result = await axios.get(
+				`https://www.omdbapi.com/?apikey=efbef9a0&t=${movieSearch}`
+			);
+			console.log(result);
+			this.setState({
+				title: result.data.Title,
+				year: result.data.Year,
+				rated: result.data.Rated,
+				genre: result.data.Genre,
+				plot: result.data.Plot,
+				language: result.data.Language,
+				country: result.data.Country,
+				awards: result.data.Awards,
+				poster: result.data.Poster,
+                runtime: result.data.Runtime,
+				isError: false,
+				errorMessage: "",
+				isLoading: false,
+			});
+		} catch (e) {
+			console.log(e.response);
+			if (e && e.response.status === 404) {
+				this.setState({
 					isError: true,
-					errorMessage: error.response.data,
+					errorMessage: e.response.data,
 					isLoading: false,
 				});
-            }
-        }
-    }
+			}
+		}
+	};
 
-    onChangeHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+	onChangeHandler = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
 
-    onSubmitHandler = () => {
-        this.fetchMovies(this.state.movieSearch)
-    }
-
+	onSubmitHandler = async() => {
+		this.fetchMovies(this.state.movieSearch);
+	};
 
 	render() {
 		return (
-            <div className="app">
-                <form>
-                    <input name="movieSearch" value={this.state.movieSearch} onChange={this.onChangeHandler}/>
-                    <button onClick={this.onSubmitHandler}>Search</button>
-                </form>
-                <div>
-                        {this.state.isError && (
-                            <span style={{ color: "red" }}>{this.state.errorMessage}</span>
-                        )}
-                    </div>
-                <div>
-                    {this.state.isLoading ? (<Loading/>) : (
-                        <MovieDetail
-                            title={this.state.title}
-                            year={this.state.year}
-                            rated={this.state.rated}
-                            genre={this.state.genre}
-                            director={this.state.director}
-                            writer={this.state.writer}
-                            plot={this.state.plot}
-                            language={this.state.language}
-                            country={this.state.country}
-                            awards={this.state.awards}
-                            poster={this.state.poster}
-                        />
-                    )}
-                </div>
-            </div>
-        );
+			<div className="app">
+				<form>
+					<input
+						name="movieSearch"
+						value={this.state.movieSearch}
+						onChange={this.onChangeHandler}
+					/>
+					<button onClick={this.onSubmitHandler}>Search</button>
+				</form>
+				<div>
+					{this.state.isError && (
+						<span style={{ color: "red" }}>{this.state.errorMessage}</span>
+					)}
+				</div>
+				<div>
+					{this.state.isLoading ? (
+						<Loading />
+					) : (
+						<MovieDetail
+							title={this.state.title}
+							year={this.state.year}
+							rated={this.state.rated}
+							genre={this.state.genre}
+							plot={this.state.plot}
+							language={this.state.language}
+							country={this.state.country}
+							awards={this.state.awards}
+							poster={this.state.poster}
+                            runtime={this.state.runtime}
+						/>
+					)}
+				</div>
+			</div>
+		);
 	}
 }
 
